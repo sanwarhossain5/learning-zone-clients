024928d9a66d1cb488ee32d888e5cd0c04bf8bcf
sanwarhossain5 <sonymix5000@gmail.com>
2022-10-27 20:23:21 +0600

    pdf added

--- src/pages/Courses/RightSide/SingleCourse.js
+++ src/pages/Courses/RightSide/SingleCourse.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
+import ReactToPdf from "react-to-pdf";
 
+const ref = React.createRef();
 const SingleCourse = () => {
     const course = useLoaderData();
 
     return (
         <Container className='pt-5' data-aos="fade-zoom-in">
-            <Card>
+            
+            <ReactToPdf targetRef={ref}
+            filename = "code-example.pdf">
+                {({ toPdf }) => <button onClick={toPdf}
+                >Generate Pdf</button>}
+            </ReactToPdf>
+            <Card ref={ref}>
+            
                 <Row>
                     <Col md={4}>
                         <Card.Img className='p-3 rounded-2' variant="top" src={course.picture} />
